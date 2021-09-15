1b29d8b1abeb6318ebbabbbe522431fec861ca90
josephkem <josephkemoy@gmail.com>
2021-09-15 16:18:40 -0400

    Created component for the featured product

---
+++ src/components/shared/FeaturedProduct.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./featured-product.styles.scss";
+
+const FeaturedProduct = ({ title, imageUrl, price }) => {
+  return (
+    <div className="featured-product">
+      <div className="featured-image">
+        <img src={imageUrl} alt="product" />
+      </div>
+      <div className="name-price">
+        <h3>{title}</h3>
+        <p>$ {price}</p>
+        <button className="button is-black nomad-btn">ADD TO CART</button>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProduct;
