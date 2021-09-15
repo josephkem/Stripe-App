8648086aed41cee0ee2c452b26045635729c15ed
josephkem <josephkemoy@gmail.com>
2021-09-15 18:29:59 -0400

    Created Not Found component to render for unknown routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Switch, Route } from "react-router-dom";
 import "./App.scss";
 import Home from "./components/Home";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="*" component={NotFound} />
       </Switch>
     </div>
   );
