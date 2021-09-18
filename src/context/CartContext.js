a2daf0d2740166daf4a639ed46cb6c1788600fb3
josephkem <josephkemoy@gmail.com>
2021-09-18 14:48:58 -0400

    Modified reducer and cart context to be able to add products to the cart

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -8,8 +8,13 @@ const initialState = { cartItems: [], itemCount: 0, total: 0 };
 const CartContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(cartReducer, initialState);
 
+  const addProduct = (product) => {
+    dispatch({ type: "ADD_ITEM", payload: { product } });
+  };
+
   const contextValues = {
     ...state,
+    addProduct,
   };
 
   return (
