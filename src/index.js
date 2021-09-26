0ef179fad42a49c0d66b4cde902a42b4026e22b7
josephkem <josephkemoy@gmail.com>
2021-09-26 12:49:31 -0400

    Added stripe components to the main app

--- src/index.js
+++ src/index.js
@@ -5,20 +5,20 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import ProductsContextProvider from "./context/ProductsContext";
 import CartContextProvider from "./context/CartContext";
-import reportWebVitals from "./reportWebVitals";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(process.env.REACT_APP_PUBLISHABLE_KEY);
 
 ReactDOM.render(
   <BrowserRouter>
     <ProductsContextProvider>
       <CartContextProvider>
-        <App />
+        <Elements stripe={stripePromise}>
+          <App />
+        </Elements>
       </CartContextProvider>
     </ProductsContextProvider>
   </BrowserRouter>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
