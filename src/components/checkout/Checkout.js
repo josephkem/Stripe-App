5ebc233f58fae9e3f8fdc5301e0aa10763b7bf20
josephkem <josephkemoy@gmail.com>
2021-09-27 20:47:09 -0400

    Created function to redirect user to the checkout portal in the stripe-checkout component and hooked it up to the checkout page

--- src/components/checkout/Checkout.js
+++ src/components/checkout/Checkout.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
 import Layout from "../shared/Layout";
+import StripeCheckout from "./stripe-checkout/StripeCheckout";
 import "./checkout.styles.scss";
 
 function Checkout() {
@@ -10,7 +11,8 @@ function Checkout() {
       <div className="checkout">
         <h2>Checkout Summary</h2>
         <h3>{`Total Items: ${itemCount}`}</h3>
-        <h4>{`Amount Due: ${total}`}</h4>
+        <h4>{`Amount Due: $${total}`}</h4>
+        <StripeCheckout />
       </div>
     </Layout>
   );
--- src/components/checkout/stripe-checkout/StripeCheckout.js
+++ src/components/checkout/stripe-checkout/StripeCheckout.js
@@ -6,6 +6,38 @@ import { fetchFromAPI } from "../../../helpers";
 function StripeCheckout() {
   const [email, setEmail] = useState("");
   const { cartItems } = useContext(CartContext);
+  const stripe = useStripe();
+  const handleGuestCheckout = async (e) => {
+    e.preventDefault();
+
+    const line_items = cartItems.map((item) => {
+      return {
+        quantity: item.quantity,
+        price_data: {
+          currency: "usd",
+          unit_amount: item.price * 100, //amount is in cents
+          product_data: {
+            name: item.title,
+            description: item.description,
+            images: [item.imageUrl],
+          },
+        },
+      };
+    });
+
+    const response = await fetchFromAPI("create-checkout-session", {
+      body: { line_items, customer_email: email },
+    });
+
+    const { sessionId } = response;
+    const { error } = await stripe.redirectToCheckout({
+      sessionId,
+    });
+
+    if (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <form onSubmit={handleGuestCheckout}>
@@ -19,7 +51,9 @@ function StripeCheckout() {
         />
       </div>
       <div className="submit-btn">
-        <button className="button is-black nomad-btn submit">Checkout</button>
+        <button className="button is-black nomad-btn submit" type="submit">
+          Checkout
+        </button>
       </div>
     </form>
   );
