9bda6d60cda4cd45e797321d802846fe33926e1e
josephkem <josephkemoy@gmail.com>
2021-10-06 15:25:13 -0400

    Plugged the custom chckout component into the checkout page and tested it

--- src/components/checkout/Checkout.js
+++ src/components/checkout/Checkout.js
@@ -3,13 +3,17 @@ import { CartContext } from "../../context/CartContext";
 import Layout from "../shared/Layout";
 import StripeCheckout from "./stripe-checkout/StripeCheckout";
 import ShippingAddress from "./custom-checkout/ShippingAddress";
+import CustomCheckout from "./custom-checkout/CustomCheckout";
 import "./checkout.styles.scss";
 
 function Checkout() {
-  const { itemCount, total } = useContext(CartContext);
+  const { itemCount, total, cartItems } = useContext(CartContext);
   const [shipping, setShipping] = useState(null);
   const addressShown = {
     display: shipping ? "none" : "black",
+  };
+  const cardShown = {
+    display: shipping ? "block" : "none",
   };
   return (
     <Layout>
@@ -19,6 +23,9 @@ function Checkout() {
         <h4>{`Amount Due: $${total}`}</h4>
         <div style={addressShown}>
           <ShippingAddress setShipping={setShipping} />
+        </div>
+        <div style={cardShown}>
+          <CustomCheckout {...{ shipping, cartItems }} />
         </div>
       </div>
     </Layout>
