f92c54191ce34d9c93a1c50008340682a0ba4eda
josephkem <josephkemoy@gmail.com>
2021-10-06 16:28:41 -0400

    Added new webhook to test and verify the custo checkout

--- server/api/webhook.js
+++ server/api/webhook.js
@@ -1,5 +1,18 @@
 const stripeAPI = require("../stripe");
 
+webHookHandlers = {
+  "checkout.session.completed": (data) => {
+    console.log("checkout completed succesfully", data);
+    //other business logic
+  },
+  "payment_intent.succeeded": (data) => {
+    console.log("Payment succeeded", data);
+  },
+  "payment_intent.payment_failed": () => {
+    console.log("Payment Failed", data);
+  },
+};
+
 function webhook(req, res) {
   const sig = req.headers["stripe-signature"];
 
@@ -15,10 +28,8 @@ function webhook(req, res) {
     return res.status(400).send(`Webhook error ${error.message}`);
   }
 
-  if (event.type === "checkout.session.completed") {
-    const session = event.data.object;
-
-    console.log("Event data", session);
+  if (webHookHandlers[event.type]) {
+    webHookHandlers[event.type](event.data.object);
   }
 }
 
