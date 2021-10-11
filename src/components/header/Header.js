3479d6be747f16a7024994983e3d4e00d9e8b3da
josephkem <josephkemoy@gmail.com>
2021-10-11 18:52:11 -0400

    Added conditional statements to render the authentication links on the header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import CartIcon from "../cart-icon/CartIcon";
+import { auth } from "../../firebase/index";
+import { UserContext } from "../../context/UserContext";
 import "./header.styles.scss";
 
 function Header() {
+  const { user } = useContext(UserContext);
+  console.log(user);
   return (
     <nav className="nav-menu container">
       <div className="logo">
@@ -16,6 +20,17 @@ function Header() {
         <li>
           <Link to="/shop">Shop</Link>
         </li>
+        {!user && (
+          <li>
+            <Link to="/sign-in">Sign In</Link>
+          </li>
+        )}
+        {user && <li onClick={() => auth.signOut()}>Sign Out</li>}
+        {!user && (
+          <li>
+            <Link to="/sign-up">Sing Up</Link>
+          </li>
+        )}
       </ul>
       <CartIcon />
     </nav>
